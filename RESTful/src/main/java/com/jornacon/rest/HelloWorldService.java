ac3d6ab665b194dc45159684ce6c7a35af8ffe88
Jordi <jornacon@gmail.com>
2014-05-18 14:51:04 +0200

    RESTful first commit

--- RESTful/src/main/java/com/jornacon/rest/HelloWorldService.java
+++ RESTful/src/main/java/com/jornacon/rest/HelloWorldService.java
@@ -1,9 +1,21 @@
 package com.jornacon.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
 /**
  * Project name: JAXRS
  * Created by: jornacon
  * On date: 18/05/14
  */
+@Path("/hello")
 public class HelloWorldService {
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg){
+        String output = "Jersey say: " + msg;
+        return Response.status(200).entity(output).build();
+    }
 }
