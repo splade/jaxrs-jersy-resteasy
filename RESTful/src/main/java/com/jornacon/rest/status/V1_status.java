e68100132c4798e222c67a8652c39090a9de7301
jornacon <jornacon@gmail.com>
2014-05-22 17:13:01 +0200

    Finished episode 2

---
+++ RESTful/src/main/java/com/jornacon/rest/status/V1_status.java
@@ -0,0 +1,28 @@
+package com.jornacon.rest.status;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created by User on 22/05/2014.
+ */
+@Path("/v1/status/*")
+public class V1_status {
+
+    private static final String api_version = "00.01.00";
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String returnTitle(){
+        return "<p>Java Web Service</p>";
+    }
+
+    @Path("/version")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String returnVersion(){
+        return "<p>Version:</p>" + api_version;
+    }
+}
